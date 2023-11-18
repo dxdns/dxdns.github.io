bd4acf6c24a80a09ca70dab789df5af3f07798fc
dxdns <dxdns@hotmail.com>
2023-11-18 19:20:17 -0300

    app pub modal v1

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var closeModalBtn = document.getElementById("closeModalBtn")
+var modal = document.getElementById("myModal")
+var modalPreference = localStorage.getItem("modalPreference")
+
+if (modalPreference !== "closed") {
+    modal.style.display = "block"
+} else {
+    modal.style.display = "none"
+}
+
+var countdown = 10
+
+closeModalBtn.addEventListener("click", function () {
+    if (countdown <= 0) {
+        modal.style.display = "none"
+        localStorage.setItem("modalPreference", "closed")
+    }
+})
+
+closeModalBtn.disabled = true
+
+var countdownInterval = setInterval(function () {
+    countdown--
+
+    if (countdown >= 0) {
+        closeModalBtn.innerText = countdown
+    } else {
+        clearInterval(countdownInterval)
+        closeModalBtn.innerText = "×"
+        closeModalBtn.disabled = false
+    }
+}, 1000)
